e50feb56c70b67dfaee9ed60e954f1ed1406d312
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-20 22:00:56 +0900

    GunController 완성

---
+++ GunController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraPos.x));
+        Vector3 target = new Vector3(0f, mousePos.y, mousePos.z);
+        transform.LookAt(target);
+    }
+}
