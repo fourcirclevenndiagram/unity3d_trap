0411acf459078e7dbf08571561efd4305c191db6
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-21 17:29:13 +0900

    GunController 수정. 총 쏘면 총알 나오게.

--- GunController.cs
+++ GunController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
+    [SerializeField] ParticleSystem ps_MuzzleFlash;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,26 @@ public class GunController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        TryFire();
+        LockOnMouse();
+
+    }
+    void TryFire()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Fire();
+
+        }
+    }
+    void Fire()
+    {
+        Debug.Log("총알 발사");
+        ps_MuzzleFlash.Play();
+
+    }
+    void LockOnMouse()
     {
         Vector3 cameraPos = Camera.main.transform.position;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraPos.x));
