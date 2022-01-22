2feb98b5a2f05de8c8023619810905d358c2a3ff
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-23 07:47:51 +0900

    배경음, 효과음에 관련된 스크립트 작성

--- Bullet.cs
+++ Bullet.cs
@@ -10,9 +10,13 @@ public class Bullet : MonoBehaviour
     [Header("총알 데미지")]
     [SerializeField] int damage;
 
+    [Header("피격 효과음")]
+    [SerializeField] string sound_Ricochet;
+
     void OnCollisionEnter(Collision other) // 다른 컬라이더와 충돌하는 순간 호출되는 함수, 충돌한 객체의 정보는 other에 담김
     {
         ContactPoint contactPoint = other.contacts[0]; // 충돌한 객체의 접촉면 정보가 담김
+        SoundManager.instance.PlaySE(sound_Ricochet);
 
         var clone = Instantiate(go_RicochetEffect, contactPoint.point, Quaternion.LookRotation(contactPoint.normal)); 
                                                         // Quaternion.LookRotation : 특정 방향을 바라보게 만드는 메서드. / normal : 충돌한 컬라이더의 표면 방향
