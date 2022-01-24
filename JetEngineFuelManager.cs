d8ea8949a5bce015e96be7880c8280dd4a879dfa
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-24 16:24:10 +0900

    제트엔진연료매니저 스크립트 수정 : 연료 자동 충전, 코드 다듬기

--- JetEngineFuelManager.cs
+++ JetEngineFuelManager.cs
@@ -8,6 +8,11 @@ public class JetEngineFuelManager : MonoBehaviour
     [SerializeField] float maxFuel; // 최대 연료
     float currentFuel; // 현재 연료
 
+    [SerializeField] float waitRechargeFuel; // 재충전 대기 시간
+    float currentWaitRechargeFuel; // 계산
+
+    [SerializeField] float rechargeSpeed; // 재충전속도
+
     [SerializeField] Slider slider_JetEngine;
     [SerializeField] Text txt_JetEngine;
     // Start is called before the first frame update
@@ -26,17 +31,43 @@ public class JetEngineFuelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckFuel();
+        UsedFuel();
+
+        slider_JetEngine.value = currentFuel;
+           txt_JetEngine.text = Mathf.Round(currentFuel / maxFuel * 100f).ToString() + " %";
+    }
+    void CheckFuel()
+    {
         if(currentFuel > 0)
             IsFuel = true;
         else
             IsFuel = false;
+    }
+    void UsedFuel()
+    {
         if(thePC.IsJet)
         {
             currentFuel -= Time.deltaTime; // 1초에 1씩 감소
+            currentWaitRechargeFuel = 0;
             if(currentFuel <= 0)
                 currentFuel = 0;
-            slider_JetEngine.value = currentFuel;
-            txt_JetEngine.text = Mathf.Round(currentFuel / maxFuel * 100f).ToString() + " %";
+
+        }
+        else
+        {
+            FuelRecharge();
+        }
+    }
+    void FuelRecharge()
+    {
+        if(currentFuel < maxFuel)
+        {
+            currentWaitRechargeFuel += Time.deltaTime;
+            if(currentWaitRechargeFuel >= waitRechargeFuel)
+            {
+                currentFuel += rechargeSpeed * Time.deltaTime;
+            }
         }
     }
 }
