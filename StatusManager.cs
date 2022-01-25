db9a27c4299e69ea341b855200e074db6135131f
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-25 22:40:43 +0900

    스테이터스 매니저 스크립트 작성

---
+++ StatusManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusManager : MonoBehaviour
+{
+    [SerializeField] float blinkSpeed;
+    [SerializeField] int blinkCount;
+
+    [SerializeField] MeshRenderer mesh_PlayerGraphics;
+
+    public void DecreaseHp(int _num)
+    {
+        StartCoroutine(BlinkCoroutine());
+    }
+
+    IEnumerator BlinkCoroutine()
+    {
+        for(int i = 0; i < blinkCount * 2; i++)
+        {
+            mesh_PlayerGraphics.enabled = !mesh_PlayerGraphics.enabled;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+}
