e722a646367090ebd70e3f26cc8a5604fc796216
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-28 20:11:58 +0900

    지뢰 피격처리

--- Mine.cs
+++ Mine.cs
@@ -18,15 +18,5 @@ public class Mine : MonoBehaviour
 
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
