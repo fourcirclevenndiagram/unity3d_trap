cbfc25e5a911854a240006631e9e6de37845ed84
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-29 19:52:02 +0900

    지뢰 스크립트에 폭발 구현 함수 작성, 지뢰에 체력 설정

--- Mine.cs
+++ Mine.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
+    [SerializeField] int hp;
     [SerializeField] int damage;
     [SerializeField] GameObject go_EffectPrefab;
 
@@ -12,11 +13,22 @@ public class Mine : MonoBehaviour
         if(other.transform.name == "Player") // Hierarchy에서 취급하는 이름을 참조
         {
             other.transform.GetComponent<StatusManager>().DecreaseHp(damage);
-            GameObject clone = Instantiate(go_EffectPrefab, transform.position, Quaternion.identity);
-            Destroy(clone, 2f);
-            Destroy(gameObject);
+            Explosion();
 
         }
+    }
+
+    public void Damaged(int _num)
+    {
+        hp -= _num;
+        if(hp <= 0)
+            Explosion();
+    }
+    void Explosion()
+    {
+        GameObject clone = Instantiate(go_EffectPrefab, transform.position, Quaternion.identity);
+        Destroy(clone, 2f);
+        Destroy(gameObject);
     }
 
 }
