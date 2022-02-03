bfa1bd7ac85a5e272548c3dea746b1340d20c0e5
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-03 17:53:39 +0900

    아이템 스크립트에 총알 관련 쿄드 작성_2

--- PickUpItem.cs
+++ PickUpItem.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
+    [SerializeField] Gun[] guns;
+
+    const int NORMAL_GUN = 0;
     void OnTriggerEnter(Collider other)
     {
         if(other.transform.CompareTag("Item"))
@@ -14,7 +17,12 @@ public class PickUpItem : MonoBehaviour
             {
                 SoundManager.instance.PlaySE("Score");
                 // Debug.Log(item.extraScore + "를 획득했습니다.");
-                ScoreManager.extraScore += item.extraScore;
+                ScoreManager.extraScore += item.extraScore;                                
+            }
+            else if(item.itemType == ItemType.NormalGun_Bullet)
+            {
+                SoundManager.instance.PlaySE("Score");
+                guns[NORMAL_GUN].bulletCount += item.extraBullet;
             }
             Destroy(other.gameObject);
         }
