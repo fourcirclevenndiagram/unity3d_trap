e5782cba6106522a0f98f9bb8e55b37d60890d0c
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-05 21:46:03 +0900

    점수 획득에 따른 플로팅텍스트 표시 스크립트 작성

--- PickUpItem.cs
+++ PickUpItem.cs
@@ -20,18 +20,25 @@ public class PickUpItem : MonoBehaviour
         {
             Item item = other.GetComponent<Item>();
 
+            int extra = 0;
+
             if(item.itemType == ItemType.Score)
             {
                 SoundManager.instance.PlaySE("Score");
+                extra = item.extraScore;
                 // Debug.Log(item.extraScore + "를 획득했습니다.");
-                ScoreManager.extraScore += item.extraScore;                                
+                // ScoreManager.extraScore += item.extraScore;                                
+                ScoreManager.extraScore += extra;
             }
             else if(item.itemType == ItemType.NormalGun_Bullet)
             {
                 SoundManager.instance.PlaySE("Bullet");
-                guns[NORMAL_GUN].bulletCount += item.extraBullet;
+                extra = item.extraScore;
+                guns[NORMAL_GUN].bulletCount += extra;
                 theGC.BulletUiSetting();
             }
+            string message = "+" + extra;
+            FloatingTextManager.instance.CreateFloatingText(other.transform.position, message);
             Destroy(other.gameObject);
         }
     }
