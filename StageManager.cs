b9b2b03d61275ef4caa2ca9146ce25b61d79344f
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-06 21:11:03 +0900

    클리어 후 물리적용 off 스크립트 작성

--- StageManager.cs
+++ StageManager.cs
@@ -9,8 +9,12 @@ public class StageManager : MonoBehaviour
     [SerializeField] GameObject go_UI;
     [SerializeField] ScoreManager theSM;
 
+    [SerializeField] Rigidbody playerRigid;
+
     public void ShowClearUI()
     {
+        PlayerController.canMove = false;
+        playerRigid.isKinematic = true;
         go_UI.SetActive(true);
         txt_CurrentScore.text = string.Format("{0:000,000}", theSM.GetCurrentScore());
     }
