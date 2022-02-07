d4cfa7269b647e2c571d82c89e5eb4a5dfba2d45
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-07 15:15:12 +0900

    다음 스테이지로 넘어가는 기능 구현

--- StageManager.cs
+++ StageManager.cs
@@ -8,8 +8,11 @@ public class StageManager : MonoBehaviour
     [SerializeField] Text txt_CurrentScore;
     [SerializeField] GameObject go_UI;
     [SerializeField] ScoreManager theSM;
+    [SerializeField] GameObject[] go_Stages;
+    int currentStage = 0;
 
     [SerializeField] Rigidbody playerRigid;
+    [SerializeField] Transform tf_OriginPos;
 
     public void ShowClearUI()
     {
@@ -17,5 +20,15 @@ public class StageManager : MonoBehaviour
         playerRigid.isKinematic = true;
         go_UI.SetActive(true);
         txt_CurrentScore.text = string.Format("{0:000,000}", theSM.GetCurrentScore());
+    }
+
+    public void NextBtn()
+    {
+        PlayerController.canMove = true;
+        playerRigid.isKinematic = false;
+        playerRigid.gameObject.transform.position = tf_OriginPos.position;
+        go_Stages[currentStage++].SetActive(false);
+        go_Stages[currentStage].SetActive(true);
+        go_UI.SetActive(false);
     }
 }
